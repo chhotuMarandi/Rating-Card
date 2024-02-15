eeb066ff6791b6f0acebfbb5949925246a63869a
chhotu marandi <cmarandi29@gmail.com>
2024-02-15 12:45:22 +0530

    js

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-const submitBtn = document.querySelector('#submit-btn');
-
-const starButtons = document.querySelectorAll('.rate-btn');
-
-starButtons.forEach((btn)=>{
-btn.addEventListener('click',(e)=>{
-  console.log(e.target.value);
-  //test branch
-})
-})
-
-submitBtn.addEventListener('click', ()=>{
-  console.log('clicked');
-})
+alert('hi')
